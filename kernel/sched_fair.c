4a492df62e00d2f85970e92e049b805689cc7097
Lin Ming <ming.m.lin@intel.com>
2011-07-22 09:14:31 +0800

    sched: Cleanup duplicate local variable in [enqueue|dequeue]_task_fair
    No need to define a new "cfs_rq" variable in the "for" block.
    Just use the one at the top of the function.
    
    Signed-off-by: Lin Ming <ming.m.lin@intel.com>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/r/1311297271.3938.1352.camel@minggr.sh.intel.com
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    
    Conflicts:
    	kernel/sched_fair.c

--- kernel/sched_fair.c
+++ kernel/sched_fair.c
@@ -1319,7 +1319,6 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 
 	for_each_sched_entity(se) {
 		cfs_rq = cfs_rq_of(se);
-		cfs_rq->h_nr_running++;
 
 		update_cfs_load(cfs_rq, 0);
 		update_cfs_shares(cfs_rq);
@@ -1365,7 +1364,6 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 
 	for_each_sched_entity(se) {
 		cfs_rq = cfs_rq_of(se);
-		cfs_rq->h_nr_running--;
 
 		update_cfs_load(cfs_rq, 0);
 		update_cfs_shares(cfs_rq);
