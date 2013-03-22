814e5729e7e8d68b867bca418d494e882ecfab63
Grarak <williye97@gmail.com>
2013-03-22 18:02:34 +0100

    Dualboot Updates; Remove Cleaner; Toolchain changed; ADB Fix

--- mkboot/img.sh
+++ mkboot/img.sh
@@ -1,9 +1,3 @@
 echo "making ramdisk"
-cd boot.img-ramdisk
-chmod 750 init* charger
-chmod 644 default.prop
-chmod 640 fstab.n1
-chmod 644 ueventd*
-cd ..
 ./mkbootfs boot.img-ramdisk | gzip > ramdisk.gz
 ./mkbootimg --kernel zImage --ramdisk ramdisk.gz -o boot.img --base 10000000 
--- out/ButterKernel/cleaner/cleaner.sh
+++
@@ -1,4 +0,0 @@
-#!/sbin/sh
-rm -rf /data/data/com.liquid.control/shared_prefs
-rm -rf /data/dalvik-cache
-rm -rf /cache/dalvik-cache
