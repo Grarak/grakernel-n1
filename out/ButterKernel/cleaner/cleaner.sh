975108792385d6d6d2d72f02c62571f9dc6243cc
Grarak <williye97@gmail.com>
2013-04-29 21:59:27 +0200

    new cleaner script

--- out/ButterKernel/cleaner/cleaner.sh
+++ out/ButterKernel/cleaner/cleaner.sh
@@ -1,4 +1,16 @@
 #!/sbin/sh
-rm -rf /data/data/com.liquid.control/shared_prefs
+
+# clean cache
 rm -rf /data/dalvik-cache
 rm -rf /cache/dalvik-cache
+
+# clean modules
+rm -rf /system/modules
+rm -rf /system/lib/modules
+
+# CM Default
+rm -f $INITD_DIR/00banner
+rm -f $INITD_DIR/90userinit
+
+# clean kernel setting app shared_prefs
+rm -rf /data/data/com.liquid.control/shared_prefs
