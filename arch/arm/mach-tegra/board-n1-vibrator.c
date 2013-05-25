968b342648fd2bb87d3bba19284087d981eaa671
Adam77Root <adam77root@gmail.com>
2013-05-25 11:01:42 +0200

    TSP: Don't have two timed_outputs with the same name
    Change-Id: I5960de8a6cae69e5614d694b8ddef5389e1e15ed

--- arch/arm/mach-tegra/board-n1-vibrator.c
+++ arch/arm/mach-tegra/board-n1-vibrator.c
@@ -174,7 +174,7 @@ static void n1_vibrator_enable(struct timed_output_dev *dev, int value)
 }
 
 static struct timed_output_dev to_dev = {
-	.name		= "vibrator",
+	.name		= "vibrator_mach",
 	.get_time	= n1_vibrator_get_time,
 	.enable		= n1_vibrator_enable,
 };
