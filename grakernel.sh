3a38ec6fcc408f7acc2a7d12d5fd07b2776e2620
Willi Ye <williye97@gmail.com>
2013-08-13 10:43:09 +0200

    buildscript: fix derp
    Signed-off-by: Willi Ye <williye97@gmail.com>

--- grakernel.sh
+++ grakernel.sh
@@ -99,7 +99,7 @@ if [ -e arch/arm/boot/zImage ]; then
         find -name '*.ko' -exec cp -v {} out/GraKernel/system/lib/modules \;
 
         cd out/GraKernel/META-INF/com/google/android
-        sed -i s/"Version * "/"Version ${gkversion}"/ updater-script
+        sed -i s/"Version * "/"Version ${gkversion} "/ updater-script
         cd ../../../..
         rm -rf *.zip
         zip -r GraKernel_${gkversion}.zip cleaner META-INF system boot.img
