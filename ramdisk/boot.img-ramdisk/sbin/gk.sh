1c67a22deba7f07e429b66d5fd82b9e42958d9ae
Willi Ye <williye97@gmail.com>
2014-02-23 12:45:53 +0100

    ramdisk: define some defaults
    Signed-off-by: Willi Ye <williye97@gmail.com>

--- ramdisk/boot.img-ramdisk/sbin/gk.sh
+++ ramdisk/boot.img-ramdisk/sbin/gk.sh
@@ -9,8 +9,18 @@ ln -s /lib/modules/* /system/lib/modules/
 for i in /sys/block/*/queue/add_random;do echo 0 > $i;done
 
 echo interactive > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo 2 > /sys/devices/system/cpu/sched_mc_power_savings
 
-[ -d /system/app/Synapse.apk ] || mv -f /res/synapse/Synapse.apk /system/app/ && chmod 644 /system/app/Synapse.apk
+echo 6 > /sys/class/misc/voodoo_sound/headphone_eq_b1_gain
+echo 9 > /sys/class/misc/voodoo_sound/headphone_eq_b2_gain
+echo 5 > /sys/class/misc/voodoo_sound/headphone_eq_b3_gain
+echo 9 > /sys/class/misc/voodoo_sound/headphone_eq_b4_gain
+echo 8 > /sys/class/misc/voodoo_sound/headphone_eq_b5_gain
+echo 0 > /sys/class/misc/voodoo_sound/digital_gain
+echo 14 > /sys/class/misc/voodoo_sound/stereo_expansion_gain
+
+echo 0 > /sys/class/misc/fsynccontrol/fsync_enabled
+echo 1 > /sys/kernel/mm/ksm/run
 
 ln -s /res/synapse/uci /sbin/uci
 /sbin/uci
